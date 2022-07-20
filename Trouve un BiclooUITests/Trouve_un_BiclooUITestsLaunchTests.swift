10b92f73ad1dd8c1a31c0d80efff2b2cec96b4c8
Kevin JAN <najrpk@gmail.com>
2022-07-20 06:35:57 +0200

    Initial Commit

---
+++ Trouve un BiclooUITests/Trouve_un_BiclooUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Trouve_un_BiclooUITestsLaunchTests.swift
+//  Trouve un BiclooUITests
+//
+//  Created by kevin jan on 20/07/2022.
+//
+
+import XCTest
+
+class Trouve_un_BiclooUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
